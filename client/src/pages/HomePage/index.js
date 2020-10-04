6529a072f4506148c5083780a05b4de553e6785d
Ajay Kristipati <ajayk111@users.noreply.github.com>
2020-10-03 21:08:44 -0400

    sort list by distance from user

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -18,10 +18,29 @@ const styles = () => {
   };
 };
 
-const HomePage = ({ classes }) => {
-  // Trail:  {DISTANCE, DIST_TYPE, GAIN, HIGHEST, LATITUDE, LONGITUDE, RATING, RATING_COUNT, REGION, REPORT_COUNT, REPORT_DATE, TITLE, URL}
-  let [trails, setTrails] = useState([]);
 
+
+
+
+const HomePage = ({ classes }) => {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(locationSuccess);
+   }
+  }
+
+  const locationSuccess = (position) => {
+    setState({coords: position.coords, trails:state.trails});
+
+  }
+
+
+  // Trail:  {DISTANCE, DIST_TYPE, GAIN, HIGHEST, LATITUDE, LONGITUDE, RATING, RATING_COUNT, REGION, REPORT_COUNT, REPORT_DATE, TITLE, URL}
+  let [state, setState] = useState({trails: [], coords:{latitude: 0, longitude: 0}});
+  state.trails.sort((a,b) =>
+    ((a.LATITUDE - state.coords.latitude)**2 + (a.LONGITUDE - state.coords.longitude)**2) -
+    ((b.LATITUDE - state.coords.longitude)**2 + (b.LONGITUDE - state.coords.longitude)**2)
+  );
   useEffect(() => {
     const fetchData = async () => {
       const querySnapshot = await firebase
@@ -35,18 +54,26 @@ const HomePage = ({ classes }) => {
       querySnapshot.docs.forEach((doc) => {
         trails.push(doc.data());
       });
-      setTrails(trails);
+      setState({coords:state.coords,trails:trails});
     };
     fetchData();
-  }, []);
+  }, [state.coords]);
 
+
+  //console.log(trails[0]);
   return (
-    <div className={classes.page}>
+    <>
+    <button onClick={getLocation}>Get My Location</button>
+
+    <div style={{"display": "flex", "flexDirection":"column"}} className={classes.page}>
       <CssBaseline />
-      {trails.map((trail) => 
-        <Typography>{trail.TITLE}, {trail.RATING_COUNT + trail.REPORT_COUNT}</Typography>
+
+      {state.trails.map((trail) =>
+        <Typography key={trail.TITLE}>{trail.TITLE}, {trail.RATING_COUNT + trail.REPORT_COUNT}</Typography>
       )}
-    </div>
+
+  </div>
+  </>
   );
 };
 
